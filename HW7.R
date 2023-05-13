215e769359c2d9b0be287d9c43ea63801cfd341a
nusmith <nusmith@uw.edu>
2023-05-13 13:31:27 -0700

    fix problem 1

--- HW7.R
+++ HW7.R
@@ -4,20 +4,7 @@
 # (Sepal.Length, Sepal.Width, Petal.Length). Using R's built in iris dataset.
 
 # a Linear fits from given data 
-fit_slen = lm(Petal.Width ~ Sepal.Length, iris)
-fit_swid = lm(Petal.Width ~ Sepal.Width, iris)
-fit_plen = lm(Petal.Width ~ Petal.Length, iris)
-fit_slen$coefficients
-fit_swid$coefficients
-fit_plen$coefficients
-x = seq(0, 10, 0.1)
-plot(iris$Sepal.Length, iris$Petal.Width)
-lines(x, fit_slen$coefficients[1] + fit_slen$coefficients[2]*x, col="red")
-plot(iris$Sepal.Width, iris$Petal.Width)
-lines(x, fit_swid$coefficients[1] + fit_swid$coefficients[2]*x, col="red")
-plot(iris$Petal.Length, iris$Petal.Width)
-lines(x, fit_plen$coefficients[1] + fit_plen$coefficients[2]*x, col="red")
-
+fit_iris = lm(Petal.Width ~ Sepal.Length + Sepal.Width + Petal.Length, iris)
 
 # b Bootstrapping linear fits
 B = 10000
@@ -29,67 +16,50 @@ for (i in 1:B) {
   indices_BT = sample(n, n, replace = T)
   iris_BT = iris[indices_BT, 1:4]
   # Get coefficients from linear model fit
-  fit_slen_BT = lm(Petal.Width ~ Sepal.Length, iris_BT)$coefficients
-  fit_swid_BT = lm(Petal.Width ~ Sepal.Width, iris_BT)$coefficients
-  fit_plen_BT = lm(Petal.Width ~ Petal.Length, iris_BT)$coefficients
-  # currently using a BT sample for the first intercept (sepal length) only
-  coeff_BT_emp[i, 1] = fit_slen_BT[1]
-  coeff_BT_emp[i, 2] = fit_slen_BT[2]
-  coeff_BT_emp[i, 3] = fit_swid_BT[2]
-  coeff_BT_emp[i, 4] = fit_plen_BT[2]
+  fit_iris_BT = lm(Petal.Width ~ Sepal.Length + Sepal.Width + Petal.Length, iris_BT)$coefficients
+  coeff_BT_emp[i, 1] = fit_iris_BT[1]
+  coeff_BT_emp[i, 2] = fit_iris_BT[2]
+  coeff_BT_emp[i, 3] = fit_iris_BT[3]
+  coeff_BT_emp[i, 4] = fit_iris_BT[4]
 }
-colnames(coeff_BT_emp) = c("Sepal.Length Int.", "Sepal.Length Slope", 
+colnames(coeff_BT_emp) = c("Intercept", "Sepal.Length Slope", 
                        "Sepal.Width Slope", "Petal.Length Slope")
-var_coeff_BT_emp = var(coeff_BT)
+var_coeff_BT_emp = var(coeff_BT_emp)
 
 # RESIDUAL
 coeff_BT_res = matrix(NA, nrow=B, ncol = 4)
-slen_pred = predict(fit_slen)
-swid_pred = predict(fit_swid)
-plen_pred = predict(fit_plen)
+p0 = predict(fit_iris)
 for (i in 1:B){
   # BT indices for samples
   indices_BT = sample(n, n, replace = T)
-  # Get residuals for each of the three models
-  res_slen = fit_slen$residuals[indices_BT]
-  res_swid = fit_swid$residuals[indices_BT]
-  res_plen = fit_plen$residuals[indices_BT]
-  # BT Y*=predicted Petal.Width for each of the three models
-  y_slen = slen_pred + res_slen
-  y_swid = swid_pred + res_swid
-  y_plen = plen_pred + res_plen
+  # Get residuals 
+  res = fit_iris$residuals[indices_BT]
+  # BT Y*=predicted Petal.Widt
+  y_BT = p0 + res
   # Get coefficients from linear model fit
-  fit_slen_BT = lm(y_slen ~ iris$Sepal.Length)$coefficients
-  fit_swid_BT = lm(y_swid ~ iris$Sepal.Width)$coefficients
-  fit_plen_BT = lm(y_plen ~ iris$Petal.Length)$coefficients
-  # currently using a BT sample for the first intercept (sepal length) only
-  coeff_BT_res[i, 1] = fit_slen_BT[1]
-  coeff_BT_res[i, 2] = fit_slen_BT[2]
-  coeff_BT_res[i, 3] = fit_swid_BT[2]
-  coeff_BT_res[i, 4] = fit_plen_BT[2]
+  fit_BT = lm(y_BT ~ iris$Sepal.Length + iris$Sepal.Width + iris$Petal.Length)$coefficients
+  coeff_BT_res[i, 1] = fit_BT[1]
+  coeff_BT_res[i, 2] = fit_BT[2]
+  coeff_BT_res[i, 3] = fit_BT[3]
+  coeff_BT_res[i, 4] = fit_BT[4]
 }
-colnames(coeff_BT_res) = c("Sepal.Length Int.", "Sepal.Length Slope", 
+colnames(coeff_BT_res) = c("Intercept", "Sepal.Length Slope", 
                            "Sepal.Width Slope", "Petal.Length Slope")
 var_coeff_BT_res = var(coeff_BT_res)
 
 # WILD
 coeff_BT_wild = matrix(NA, nrow=B, ncol = 4)
 for (i in 1:B){
-  # BT Y*=predicted Petal.Width for each of the three models
-  y_slen = slen_pred + fit_slen$residuals*rnorm(n)
-  y_swid = swid_pred + fit_swid$residuals*rnorm(n)
-  y_plen = plen_pred + fit_plen$residuals*rnorm(n)
+  # BT Y*=predicted Petal.Width 
+  y_BT = p0 + fit_iris$residuals*rnorm(n)
   # Get coefficients from linear model fit
-  fit_slen_BT = lm(y_slen ~ iris$Sepal.Length)$coefficients
-  fit_swid_BT = lm(y_swid ~ iris$Sepal.Width)$coefficients
-  fit_plen_BT = lm(y_plen ~ iris$Petal.Length)$coefficients
-  # currently using a BT sample for the first intercept (sepal length) only
-  coeff_BT_wild[i, 1] = fit_slen_BT[1]
-  coeff_BT_wild[i, 2] = fit_slen_BT[2]
-  coeff_BT_wild[i, 3] = fit_swid_BT[2]
-  coeff_BT_wild[i, 4] = fit_plen_BT[2]
+  fit_BT = lm(y_BT ~ iris$Sepal.Length + iris$Sepal.Width + iris$Petal.Length)$coefficients
+  coeff_BT_wild[i, 1] = fit_BT[1]
+  coeff_BT_wild[i, 2] = fit_BT[2]
+  coeff_BT_wild[i, 3] = fit_BT[3]
+  coeff_BT_wild[i, 4] = fit_BT[4]
 }
-colnames(coeff_BT_wild) = c("Sepal.Length Int.", "Sepal.Length Slope", 
+colnames(coeff_BT_wild) = c("Intercept", "Sepal.Length Slope", 
                            "Sepal.Width Slope", "Petal.Length Slope")
 var_coeff_BT_wild = var(coeff_BT_wild)
 
@@ -98,7 +68,7 @@ var_comp = matrix(NA, nrow = 3, ncol = 4)
 var_comp[1,] = diag(var_coeff_BT_emp)
 var_comp[2,] = diag(var_coeff_BT_res)
 var_comp[3,] = diag(var_coeff_BT_wild)
-colnames(var_comp) = c("Sepal.Length Int.", "Sepal.Length Slope", 
+colnames(var_comp) = c("Intercept", "Sepal.Length Slope", 
                        "Sepal.Width Slope", "Petal.Length Slope")
 rownames(var_comp) = c("Empirical", "Residual", "Wild")
 
