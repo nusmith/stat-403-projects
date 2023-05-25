9803b31c8be6419b84f0bcbb83c3554e6477616d
nusmith <nusmith@uw.edu>
2023-05-24 23:16:30 -0700

    start hw 9

--- HW8.R
+++ HW8.R
@@ -16,9 +16,9 @@ legend("top", col=c("red", "blue", "green"), c("h=0.1", "h=0.3", "h=0.9"), lwd=2
 
 # b
 # Compare KDE w/ h=0.3 to histogram
-hist(eruptions, main ="Distribution of Eruptions", probability=T)
+hist(eruptions, main ="Distribution of Eruptions", probability=T, breaks=20)
 lines(kde_2, lwd=2, col="blue")
-legend("topleft", col=c("blue"), c("h=0.3"), lwd=2)
+legend("top", col=c("blue"), c("KDE w/ h=0.3"), lwd=2)
 
 # c
 # Construct confidence band for KDE w/ h=0.3
@@ -55,5 +55,91 @@ lines(x = kde$x, y=kde$y - qnorm(0.975)*dens_se, lwd=2, col="pink", lty="dashed"
 lines(x = kde$x, y=kde$y + qnorm(0.975)*dens_se,lwd=2, col="pink", lty="dashed")
 legend("topleft", col=c("magenta", "lightpink"), legend=c("KDE", "95% confidence band"), 
        lwd=2, cex=0.8)
+
+
+
+# -----------------------------------------------------------------------------------------------
+# Problem 3
+# MC simulation to analyze error of KDE
+# Target density p: double exponential distribution
+p = function(x){
+  0.5*exp(-1*abs(x))
+}
+x = seq(-1,1,0.1)
+plot(x, p(x), xlim=c(-1,1), ylim=c(0,1.1))
+# CDF of target density P
+P = function(x){
+  l = length(x)
+  y = rep(NA, l)
+  for (i in 1:l){
+    if (x[i] < 0){
+      y[i]=0.5*exp(x[i])
+    }
+    else {
+      y[i]=1-0.5*exp(-1*x[i])
+    }
+  }
+  y
+}
+lines(x, P(x))
+# Inverse of CDF of target density
+# takes in a u(0,1) RV and get the +/- from the sign of u-0.5
+P_inv = function(x){
+  l = length(x)
+  y = rep(NA, l)
+  for (i in 1:l){
+    if (x[i] < 0){
+      y[i]= log(2*x[i])
+    }
+    else {
+      y[i]= -log(-2*(x[i]-1))
+    }
+  }
+  y
+}
+# takes in a u ~ Unif(0,1) sample
+P_inv = function(u){
+  - sign(u-0.5)*log(1-2*abs(u-0.5))
+}
+# Unif sample
+sample_u = runif(1000, min=0, max=1)
+sample_p = P_inv(sample_u)
+# Plot the histogram and overlay the true density 
+hist(sample_p, breaks=30, probability=T, main = "Histogram of p(x) samples", ylim=c(0,0.5))
+x = seq(-10,10,0.1)
+lines(x, p(x), col="magenta", lwd=2.5)
+# Make KDE from sample
+kde_de = density(sample_p, bw=0.2)
+plot(kde_de, col="blue", lwd=2, main = "Double Exponential Distribution", ylim=c(0, 0.5))
+lines(x, p(x), col="forestgreen", lwd=2)
+legend("topleft", col=c("blue", "forestgreen"), legend=c("KDE", "p(x) True"), 
+       lwd=2, cex=0.9)
+# Find Mean Integrated Square Error (MISE) for h E (0.05, 0.5)
+h_seq = seq(0.05, 0.5, 0.05)
+# MC Simulation to estimate error
+N = 10000
+mise_h_seq = rep(NA, length(h_seq))
+for (i_h in 1:length(h_seq)) {
+  h0 = h_seq[i_h]
+  kde_de_MC = matrix(NA, nrow=N, ncol=1000)
+  for (i in 1:N) {
+    # Get sample from distribution, generate KDE
+    sample_u = runif(1000, min=0, max=1)
+    sample_p = P_inv(sample_u)
+    kde_de = density(sample_p, bw=h0, n=1000, from=-6, to=6)
+    kde_de_MC[i,]=kde_de$y
+  }
+  true_p = p(kde_de$x)
+  # Get MSE
+  kde_de_mse = colSums((t(t(kde_de_MC)-true_p))^2)/N
+  # Save MISE
+  mise_h_seq[i_h] = sum(kde_de_mse)
+}
+# Plot results to see how h-value (bandwidth) affects error in KDE
+plot(x=h_seq, y=mise_h_seq, type="l", lwd=3, xlab="h (bandwidth)", ylab ="MISE",
+     main= "MISE vs h", col="purple")
+
+
+
 
 
